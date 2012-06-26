fb40f4c1e1650a36a13ca62118209d8686d8f779
Paramjit Raloowall <raloowall@gmail.com>
2012-06-26 18:39:23 -0500

    add mocking demo

---
+++ Pattern.Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Principal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatternsCore.Security;
+using Rhino.Mocks;
+
+namespace Pattern.Tests
+{
+
+
+    [TestClass()]
+    public class RhinoMocksAtPlay
+    {
+
+
+        [TestMethod()]
+        public
+        void TestMethod1()
+        {
+            var mrepo = new MockRepository();
+
+            var user = mrepo.DynamicMock<IPrincipal>();
+            var permissionChecker = new CheckUserPermissions();
+            using (mrepo.Record())
+            {
+                SetupResult.For(user.IsInRole("test")).Return(true);
+            }
+
+            using (mrepo.Playback())
+            {
+                Assert.IsTrue(permissionChecker.HaveManagerRole(user));
+            }
+        }
+    }
+}
+
+
---
+++ PatternsCore/Security/CheckUserPermission.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+namespace PatternsCore.Security
+{
+    public class CheckUserPermissions
+    {
+        public bool HaveManagerRole(IPrincipal user)
+        {
+            return user.IsInRole("test");
+        }
+    }
+
+
+}
